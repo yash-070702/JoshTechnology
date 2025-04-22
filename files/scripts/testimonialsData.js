2959e2e4a999b43e7f8a825abe114545facf68d6
Yash Kumar Aggarwal <yashaggarwal2002.ya@gmail.com>
2025-04-22 16:04:10 +0530

    changes images path

--- files/scripts/testimonialsData.js
+++ files/scripts/testimonialsData.js
@@ -1,24 +1,22 @@
 const testimonialsData = [
     {
-      companyImage: "../assets/images/main_images/Vector.png",
+      companyImage: "./assets/images/main_images/Vector.png",
       text: "Most calenders are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule",
-      userImage: "../assets/images/main_images/Ellipse_2.png",
+      userImage: "./assets/images/main_images/Ellipse_2.png",
       userName: "John Doe",
       designation: "MUI Designer"
     },
     {
-      companyImage: "../assets/images/main_images/Rectangle.png",
+      companyImage: "./assets/images/main_images/Rectangle.png",
       text: "Most calenders are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule",
-      userImage: "../assets/images/main_images/Ellipse_2.png",
-userImage: "../assets/images/main_images/Ellipse_2.png",
+      userImage: "./assets/images/main_images/Ellipse_2.png",
 userName: "John Doe",
       designation: "UI Designer"
     },
     {
-      companyImage: "../assets/images/main_images/Rectangle-1.png",
+      companyImage: "./assets/images/main_images/Rectangle-1.png",
       text: "Most calenders are designed for teams. Slate is designed for freelancers who want a simple way to plan their schedule",
-      userImage: "../assets/images/main_images/Ellipse_2.png",
-      userImage: "../assets/images/main_images/Ellipse_2.png",
+      userImage: "./assets/images/main_images/Ellipse_2.png",
       userName: "John Doe",
       designation: "UI Designer"
     }
